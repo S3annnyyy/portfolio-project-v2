54bb74a0a027ae21c65c480863c17c3ab1f76f35
Sean Yap <seanyckang@gmail.com>
2023-12-22 22:28:23 +0800

    build

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -26,7 +26,7 @@ const Hero = () => {
                     <motion.div variants={slideInFromLeft(0.5)} className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto">                
                         <span>
                             Welcome to my 
-                            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-[#68e2fa]">{" "}Portfolio Website{" "}</span>
+                            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-[#68e2fa]">{" "}Portfolio Website!{" "}</span>
                         </span>            
                     </motion.div>
 

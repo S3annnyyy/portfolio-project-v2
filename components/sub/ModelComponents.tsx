4d188ebd474c52e2f54b2f33a50477a5897a6f26
Sean Yap <seanyckang@gmail.com>
2023-12-25 16:17:51 +0800

    Added textures for macbook and iphone [Fixing texture loading issue for mobile 2]

--- components/sub/ModelComponents.tsx
+++ components/sub/ModelComponents.tsx
@@ -1,4 +1,4 @@
-import { useGLTF } from '@react-three/drei'
+import { useGLTF, Decal } from '@react-three/drei'
 import { motion } from 'framer-motion-3d'
 import { Mesh, Color, MeshBasicMaterial, TextureLoader } from "three";
 
@@ -44,12 +44,12 @@ export function Laptop({isOpen,...props}: {isOpen:boolean, screenview:string}) {
       </group>
     )
   }
-  
+
   export function Mobile({isOpen,...props}: {isOpen:boolean, screenview:string}) {
     const { nodes, materials } = useGLTF('./models/mobile.glb')
-    const screenNode = nodes.Screen as Mesh;
-    const logo = nodes.Logo as Mesh;
-    const frame = nodes.Frame as Mesh;  
+    const screenNode = nodes.Screen as THREE.Mesh;
+    const logo = nodes.Logo as THREE.Mesh;
+    const frame = nodes.Frame as THREE.Mesh;  
   
     // reverse image prop to show full front 
     const screenLoader = new TextureLoader()
@@ -64,5 +64,132 @@ export function Laptop({isOpen,...props}: {isOpen:boolean, screenview:string}) {
         <mesh geometry={logo.geometry} material={materials.Logo} />
         <mesh geometry={screenNode.geometry} material={new MeshBasicMaterial({map: screen})} rotation={[0, -Math.PI / 2, 0]} />
       </group>
+    )
+  }
+  
+  export function Mobile2({isOpen,...props}: {isOpen:boolean, screenview:string}) {
+    const { nodes, materials } = useGLTF('./models/mobile2.glb')  
+    
+    const A = nodes.UCttAeyROPsgmix as Mesh;
+    const B = nodes.XbtrdVaOWYmkEiU as Mesh;
+    const screenNode = nodes.YbXWdqEcjbfTKuN_0 as Mesh;
+    const D = nodes.rrqFqyfckTuyRuI as Mesh;
+    const E = nodes.lxsKwuOPNvmzBKg_0 as Mesh;
+    const F = nodes.alSOKOYgFKIcUtR as Mesh;
+    const G = nodes.FjhETOCBEeiBmch as Mesh;
+    const H = nodes.AbxQOpRbGREHXRG as Mesh;
+    const I = nodes.tWfjYtMZCfucxRt as Mesh;
+    const J = nodes.vauUojKrKkLLDtY as Mesh;
+    const K = nodes.yqmgDmvGsmuPwXx_0 as Mesh;
+    const L = nodes.wjSYkRykuFHJNPw as Mesh;
+    const M = nodes.rJeCWUNsVVXXAbI as Mesh;
+    const N = nodes.rqSonbcVVSPWFfa_0 as Mesh;
+    const O = nodes.OMkeKbwVHRBkBwM as Mesh;
+    const P = nodes.DLfIUIalXuQjJsL as Mesh;
+    const Q = nodes.KABLQLZRuEbcLWk as Mesh;
+    const R = nodes.qjXEDwnnBYwWcJn as Mesh;
+    const S = nodes.FGDSbHbILfUmiaH as Mesh;
+    const T = nodes.jQXfQpudiYObSGp as Mesh;
+    const U = nodes.knexoFNknstHgiO as Mesh;
+    const V = nodes.FaUtifOQSMTXiZM as Mesh;
+    const W = nodes.BhvzCWikxrVeDTV as Mesh;
+    const X = nodes.MMkajxMNWrwGQfi as Mesh;
+    const Y = nodes.YnrVhXEUDbStWCs as Mesh;
+    const Z = nodes.ePYqawqlCJbCsNi as Mesh;
+    const AA = nodes.RGbIswEcCTzqNsn_0 as Mesh;
+    const AB = nodes.IuMgFUHIyRWENxG_0 as Mesh;
+    const AC = nodes.dNDonqESZOxUcei_0 as Mesh;
+    const AD = nodes.XeFHhVBtRZWPGxR as Mesh;
+    const AE = nodes.nJYGEbPQybRBbiN as Mesh;
+    const AF = nodes.JyAbjubWrOdfygC_0 as Mesh;
+    const AG = nodes.nxFoHsySvfcSLvp as Mesh;
+    const AH = nodes.oCklTGvTZoDWJrC_0 as Mesh;
+    const AI = nodes.QaGkMzxNzKPcqRy as Mesh;
+    const AJ = nodes.KUDomTaVduCyevu as Mesh;
+    const AK = nodes.QOfJIBEXOvXfSUh as Mesh;
+    const AL = nodes.RUMRNTkptJGDMpy as Mesh;
+    const AM = nodes.aYjPeBrpBRopJbp as Mesh;
+    const AN = nodes.BeQtuLXtpSTrzAH as Mesh;
+    const AO = nodes.tWBbDznHihIxXam as Mesh;
+    const AP = nodes.PLFTnNQeqVAxicS as Mesh;
+    const AQ = nodes.GWEiavWnRxbogtw_0 as Mesh;
+    const AR = nodes.RFqaqXLpuCDBIGV_0 as Mesh;
+    const AS = nodes.JUTNJcWwqyxbGDZ_0 as Mesh;
+    const AT = nodes.BDLCJBydsNjizog_0 as Mesh;
+    const AU = nodes.zPPSOvNamLQVyvv as Mesh;
+    const AV = nodes.YPGjoywokSeoQFr as Mesh;
+    const AW = nodes.MoTJNOoMxqdxNvQ as Mesh;
+    const AX = nodes.mZxrNiCtMrMjOMv as Mesh;
+    const AY = nodes.tQCDizUpBYNcvFA as Mesh;
+    const AZ = nodes.CAQeTxdpUcbxQyT as Mesh;
+    const BA = nodes.qsTxqfACkdoWeLQ as Mesh;
+    const BB = nodes.aVmapfDgqkPkjUf as Mesh;
+    const BC = nodes.lgnGJJmNebyRbHq_0 as Mesh;
+
+  
+    // reverse image prop to show full front 
+    const screenLoader = new TextureLoader()
+    const screen = screenLoader.load(props.screenview) // load img prop here      
+   
+    return (
+      <group {...props} dispose={null} position={[0, -4, 0]} rotation={[0, Math.PI, 0]}>
+      <group scale={0.7}>
+        <mesh geometry={A.geometry} material={materials.KtvhjlxyToKjYkE} />
+        <mesh geometry={B.geometry} material={materials.YiceMpFVTpnmoaq} />
+        <mesh geometry={screenNode.geometry} material={new MeshBasicMaterial({map: screen})} />
+        <mesh geometry={D.geometry} material={materials.CSNzlRnZuvCyxNL} />
+        <mesh geometry={E.geometry} material={materials.KhJiSWFcsscOusf} />
+        <mesh geometry={F.geometry} material={materials.sWxYOtHGWTcXRMx} />
+        <mesh geometry={G.geometry} material={materials.KtvhjlxyToKjYkE} />
+        <mesh geometry={H.geometry} material={materials.IDdMwJVCyuFpUnA} />
+        <mesh geometry={I.geometry} material={materials.fdfRsQCrfvPBPfQ} />
+        <mesh geometry={J.geometry} material={materials.HvAGJeQTAiWbceX} />
+        <mesh geometry={K.geometry} material={materials.bmOZLlCkCKhIIVe} />
+        <mesh geometry={L.geometry} material={materials.KxzouvBYEgdZhMo} />
+        <mesh geometry={M.geometry} material={materials.psePdsxZprlxGrw} />
+        <mesh geometry={N.geometry} material={materials.jFPFAvCbiqflbQV} />
+        <mesh geometry={O.geometry} material={materials.IkWzRHNnDaKQXIi} />
+        <mesh geometry={P.geometry} material={materials.pBMikDFQfUOsKkr} />
+        <mesh geometry={Q.geometry} material={materials.fdfRsQCrfvPBPfQ} />
+        <mesh geometry={R.geometry} material={materials.tfbCjiZQaZkmtHx} />
+        <mesh geometry={S.geometry} material={materials.tfbCjiZQaZkmtHx} />
+        <mesh geometry={T.geometry} material={materials.sWxYOtHGWTcXRMx} />
+        <mesh geometry={U.geometry} material={materials.KtvhjlxyToKjYkE} />
+        <mesh geometry={V.geometry} material={materials.KtvhjlxyToKjYkE} />
+        <mesh geometry={W.geometry} material={materials.LtesZnUOMbBEAoi} />
+        <mesh geometry={X.geometry} material={materials.LtesZnUOMbBEAoi} />
+        <mesh geometry={Y.geometry} material={materials.EJpkIDZfhPDUzel} />
+        <mesh geometry={Z.geometry} material={materials.CSNzlRnZuvCyxNL} />
+        <mesh geometry={AA.geometry} material={materials.FlDKBWPodPcEeGy} />
+        <mesh geometry={AB.geometry} material={materials.LcWBQfBvCzxThpp} />
+        <mesh geometry={AC.geometry} material={materials.LUbRMhkIhuekQRK} />
+        <mesh geometry={AD.geometry} material={materials.tfbCjiZQaZkmtHx} />
+        <mesh geometry={AE.geometry} material={materials.tDZQoaroJfCIQtF} />
+        <mesh geometry={AF.geometry} material={materials.jFPFAvCbiqflbQV} />
+        <mesh geometry={AG.geometry} material={materials.KtvhjlxyToKjYkE} />
+        <mesh geometry={AH.geometry} material={materials.tfbCjiZQaZkmtHx} />
+        <mesh geometry={AI.geometry} material={materials.iEhZxWeNLTDdgxm} />
+        <mesh geometry={AJ.geometry} material={materials.KtvhjlxyToKjYkE} />
+        <mesh geometry={AK.geometry} material={materials.vsSJQngPxBJTVZb} />
+        <mesh geometry={AL.geometry} material={materials.LJBezuBxKRoHnAp} />
+        <mesh geometry={AM.geometry} material={materials.xHgtbqndQshkTKG} />
+        <mesh geometry={AN.geometry} material={materials.initialShadingGroup} />
+        <mesh geometry={AO.geometry} material={materials.OStzgRHtVBLWwiD} />
+        <mesh geometry={AP.geometry} material={materials.BLpVAsLWNICZYGG} />
+        <mesh geometry={AQ.geometry} material={materials.FsunUcGyajFpQmW} />
+        <mesh geometry={AR.geometry} material={materials.nJRBoEsOhzMSqCz} />
+        <mesh geometry={AS.geometry} material={materials.LJBezuBxKRoHnAp} />
+        <mesh geometry={AT.geometry} material={materials.fGwijctGaiRaYJC} />
+        <mesh geometry={AU.geometry} material={materials.qEGySvwsouNnVcn} />
+        <mesh geometry={AV.geometry} material={materials.KtvhjlxyToKjYkE} />
+        <mesh geometry={AW.geometry} material={materials.IBtgGxCVyZhjKZM} />
+        <mesh geometry={AX.geometry} material={materials.WqbAhnIPgrrhfXS} />
+        <mesh geometry={AY.geometry} material={materials.LtesZnUOMbBEAoi} />
+        <mesh geometry={AZ.geometry} material={materials.KtvhjlxyToKjYkE} />
+        <mesh geometry={BA.geometry} material={materials.IBtgGxCVyZhjKZM} />
+        <mesh geometry={BB.geometry} material={materials.WqbAhnIPgrrhfXS} />
+        <mesh geometry={BC.geometry} material={materials.rNCplyWedyfORFh} />
+      </group>
+    </group>
     )
   }
--- components/sub/ProjectComponent.tsx
+++ components/sub/ProjectComponent.tsx
@@ -3,15 +3,15 @@
 import React from 'react'
 import { useState } from "react"
 import { Canvas } from '@react-three/fiber'
-import { Laptop, Mobile } from './ModelComponents'
-import { PerspectiveCamera, OrbitControls } from '@react-three/drei'
+import { Laptop, Mobile, Mobile2 } from './ModelComponents'
+import { PerspectiveCamera, OrbitControls, Environment } from '@react-three/drei'
 
 const ProjectComponent = (props: {screen: string, platform: string}) => {
   const [isOpen] = useState(true)  
   return (
     <div className="flex flex-col items-center">
       <div className="z-10 md:w-screen md:h-screen">
-        <Canvas>
+        <Canvas shadows>
           <ambientLight intensity={1} />
           <pointLight position={[10,10,50]} />
           <mesh>           
@@ -23,12 +23,14 @@ const ProjectComponent = (props: {screen: string, platform: string}) => {
             )}
             <PerspectiveCamera makeDefault position={[0,2,20]} />
             <OrbitControls 
+              makeDefault
               enableZoom={false} 
               minAzimuthAngle={-(10 * Math.PI) / 180} // Limit horizontal rotation to -10 degrees
               maxAzimuthAngle={(10 * Math.PI) / 180}  // Limit horizontal rotation to 10 degrees
               maxPolarAngle={Math.PI/2} // Limit vertical rotation to not go below ground            
               rotateSpeed={0.2}             
             />            
+            <Environment preset="city" />            
            </mesh>
         </Canvas>
       </div>      

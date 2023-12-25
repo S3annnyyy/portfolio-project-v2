55dfadaaa21b944825cfd7d381f4baa274405715
Sean Yap <seanyckang@gmail.com>
2023-12-25 09:22:41 +0800

    Edited footer to make it evenly spaced and sized

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -4,12 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Footer = () => {
   return (
-    <div className='w-screen flex flex-col text-center md:flex-row justify-between mb-[20px] md:px-20 text-base md:text-xs text-white mt-5 mb-5'>
+    <div className='w-screen flex flex-col text-center md:flex-row justify-between mb-[20px] md:px-20 md:text-xs font-primary text-sm text-textBody mt-5 mb-5'>
         <div>Crafted by yours truly</div>
         <div>&copy; 2023 Yap Cheng Kang Sean.</div>
-        <div>
+        <div className='mt-2'>
         {Socials.map((social) => (
-                <a href={social.link} key={social.name}><FontAwesomeIcon icon={social.icon} className='mx-4 text-xl' /></a>                            
+                <a href={social.link} key={social.name}><FontAwesomeIcon icon={social.icon} className='mx-4 text-base' /></a>                            
             ))}
         </div>
     </div>
